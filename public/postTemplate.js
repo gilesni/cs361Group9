47f8128900114c8ffd9096d557aa742c0a193f52
gilesni <jklihu@gmail.com>
2018-08-09 16:17:20 -0700

    I think I made the postTemplate.js working, but I could be wrong

--- public/postTemplate.js
+++ public/postTemplate.js
@@ -3,10 +3,12 @@
 templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<article class=\"user-post\">\r\n    <div class=\"image-container-size\">\r\n        <div class=\"image-container\">\r\n            <img class=\"image-source\" src=\"https://s-media-cache-ak0.pinimg.com/236x/f8/2f/59/f82f5916dfb1370c8375654408fdc450.jpg\" />\r\n        </div>\r\n    </div>\r\n    <div class=\"user-name\">\r\n        <h5>"
-    + alias4(((helper = (helper = helpers.user || (depth0 != null ? depth0.user : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"user","hash":{},"data":data}) : helper)))
-    + "</h5>\r\n    </div>\r\n    <p class=\"user-text\">"
-    + alias4(((helper = (helper = helpers.text || (depth0 != null ? depth0.text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"text","hash":{},"data":data}) : helper)))
-    + "</p>\r\n</article>\n";
+  return "<article class=\"Post\">\r\n    <div class=\"PostTop\">\r\n        <div class=\"User\">"
+    + alias4(((helper = (helper = helpers.User || (depth0 != null ? depth0.user : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"User","hash":{},"data":data}) : helper)))
+    + "</div>\r\n <div class=\"PDate\">"
+    + alias4(((helper = (helper = helpers.PDate || (depth0 != null ? depth0.PDate : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"PDate","hash":{},"data":data}) : helper)))
+    + "</div>\r\n </div> <p class=\"PostText\">"
+    + alias4(((helper = (helper = helpers.PostText || (depth0 != null ? depth0.PostText : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"PostText","hash":{},"data":data}) : helper)))
+    + "</p>\r\n <div class=\"PostBot\">\r\n <a class=\"PostBtn\" href=\"\">Like</a>\r\n <a class=\"PostBtn\" href=\"\">Reply</a>\r\n <span>2 days</span>\r\n <a class=\"Report\" href=\"\">Report</a>\r\n </div>\r\n </article>\n";
 },"useData":true});
 })();
