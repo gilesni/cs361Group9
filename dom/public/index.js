38f16131d5292758cb174de2c1124de3b97469c7
Artem Slivka <aslivka0@gmail.com>
2018-08-09 21:48:05 -0400

    Update index.js
    Added support for sending warning popup window.

--- dom/public/index.js
+++ dom/public/index.js
@@ -1,6 +1,7 @@
 //Global variables and objects
 var postControls;
 //var postId = 0;
+var tempPost;
 
 var Post = function (){
 	this.user = "";
@@ -12,7 +13,12 @@ var Post = function (){
 	this.isFake = false;
 }
 
+
 //Functions
+//Event listeners
+window.addEventListener("message", receiveMessage);
+window.addEventListener("load", OnPageLoad);
+
 function ELByID(id){
 	return document.getElementById(id);
 }
@@ -53,14 +59,20 @@ function AddPost(){
 	//Creating post as a fragment, not part of page DOM yet
 	var newPost = CreatePost(postData);
 
-	//Add function to send form data to server via Ajax
-	
-	//Finally adding new post to page
+	//TODO: Add function to send form data to server via Ajax
+
+	//Opening warning popup
+	if(newData.isFake){
+		OpenFakeWarning("/warning.html", 400, 300);
+		tempPost = newPost;
+		return;
+	}
+	//Finally adding newly built post to page at insertLocation
 	insertAfter(newPost, insertLocation);
 }
 
 function CreatePost(postData){
-	var elements = new Array(8);
+	var elements = new Array(8);	// array of 8 DOM elements for post to be added
 	var fragment = document.createDocumentFragment();
 	var container = document.createElement("div");	
 	//Color post container based on fake status
@@ -83,6 +95,7 @@ function CreatePost(postData){
 	else {
 		container.className = "Post";	
 	}
+	//Adding elements to container , this doesn't add them to the page yet
 	for(var i = 0; i < elements.length; i++){
 		container.appendChild(elements[i]);
 	}
@@ -110,6 +123,47 @@ function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+function OpenFakeWarning(url, width, height){
+	var child = OpenWindow(url, "_blank", window, width, height); 
+
+ 	//Poll for response from child window
+    var interval = setInterval(function() {
+	    if (child.closed){
+	    	clearInterval(interval);	//Stop polling for reponse
+	        return; 
+	    }
+	    //Ask for result from child window
+	    else {
+	    	child.postMessage({ message: "requestResult" }, "*");
+	    }
+    }, 500);
+ }
+
 function OpenPostReport(){
 
+}
+function OpenWindow(url, title, win, w, h){
+	var y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
+    var x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
+    var options = 'toolbar=no, location=no, directories=no, status=no,';
+        options +='menubar=no, scrollbars=no, resizable=no, copyhistory=no,';
+        options +='width='+ w.toString() +', height='+ h.toString()+ ', ';
+        options +='top=' + y.toString() + ', left=' + x.toString();
+   var child = win.open(url, title, options);	//Opening new window
+   return child;
+}
+
+function receiveMessage(event)
+{
+  // event.source is popup, event.data is data from popup
+  //console.log("Received data from popup:");
+  if (event.data.message == "deliverResult") {
+        console.log("Warning popup button: " + event.data.result);       
+        event.source.close();
+        if(event.data.result == "Post"){
+        	//Creating the post
+        	var insertLocation = ELByID("createPost");
+        	insertAfter(tempPost, insertLocation);
+        }
+    }
 }
