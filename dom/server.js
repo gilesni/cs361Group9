3b6cda822c5b808d6f0b5ac50cd154666d69a346
gilesni <jklihu@gmail.com>
2018-08-09 17:54:48 -0700

    Split the code into two folders one for dom and one for ajax, at time being dom works and ajax does not.

---
+++ dom/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res, next) => {
+  res.status(200).sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, (err) => {
+  if (!err) {
+    console.log("Server listening on port 3000");
+  }
+});
